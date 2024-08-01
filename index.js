f9b5826f673e065802f29b1c305361fbec1ff200
heeba <heebahassan182@gmail.com>
2024-08-01 12:41:43 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide(2000,1000);
+
+const square = (parameter1) => parameter1 * parameter1;
+square(4);
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,2);
